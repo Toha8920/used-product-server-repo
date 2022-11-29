366bc5a79bffbaebf6f0d0150bd32bdd7ba84c21
Toha Islam <islamtoha710@gmail.com>
2022-11-29 10:57:12 +0600

    some changes

--- index.js
+++ index.js
@@ -53,11 +53,15 @@ async function run() {
         })
 
         app.delete('/products/:id', async (req, res) => {
+
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await productsCollection.deleteOne(query);
             res.send(result)
-        })
+
+        });
+
+
         app.patch('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
